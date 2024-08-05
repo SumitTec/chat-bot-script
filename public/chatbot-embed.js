e2b4db651a83cf5952c3611ecb16275ff232c26c
SumitSaxenaTec <ssaxena@command-center.io>
2024-08-05 14:54:30 +0530

    Change script

--- public/chatbot-embed.js
+++ public/chatbot-embed.js
@@ -4,7 +4,7 @@
   const local = window.location.hostname === "localhost";
   const scriptSrc = local
     ? "http://localhost:3000/static/js/bundle.js" // Local path
-    : "https://your-vercel-deployment-url.vercel.app/static/js/main.js"; // Update with the correct Vercel path
+    : "https://chat-bot-script.vercel.app/static/js/main.js"; // Update with the correct Vercel path
   script.src = scriptSrc;
   script.onload = function () {
     // Optionally, define the custom element here if not done in the main.js
