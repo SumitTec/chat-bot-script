38d2cbf3bed7ae9c291d078efdeb070087b6d49d
SumitSaxenaTec <ssaxena@command-center.io>
2024-08-05 14:57:42 +0530

    Change script

--- public/chatbot-embed.js
+++ public/chatbot-embed.js
@@ -1,13 +1,17 @@
-// public/chatbot-embed.js
 (function () {
   const script = document.createElement("script");
   const local = window.location.hostname === "localhost";
+  // Ensure these paths are correct
   const scriptSrc = local
-    ? "http://localhost:3000/static/js/bundle.js" // Local path
-    : "https://chat-bot-script.vercel.app/static/js/main.js"; // Update with the correct Vercel path
+    ? "http://localhost:3000/static/js/bundle.js" // Local development path
+    : "https://chat-bot-script.vercel.app/static/js/main.js"; // Path for Vercel deployment
+
   script.src = scriptSrc;
   script.onload = function () {
-    // Optionally, define the custom element here if not done in the main.js
+    // Optionally, define the custom element here if not done in main.js
+  };
+  script.onerror = function () {
+    console.error("Failed to load the chatbot script.");
   };
   document.head.appendChild(script);
 })();
